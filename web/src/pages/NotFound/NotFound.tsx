90b39f3160d62f839a2727284ef87090cb419672
CrispenGari <Crispengari@gmail.com>
2022-11-16 17:39:25 +0200

    i fogort not found

--- web/src/pages/NotFound/NotFound.tsx
+++ web/src/pages/NotFound/NotFound.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./NotFound.css";
 interface Props {}
-const NotFound: React.FC<Props> = ({}) => {
+const NotFound: React.FC<Props> = () => {
   return (
-    <div className="NotFound">
-      <h1>Hello from NotFound</h1>
+    <div className="not__found">
+      <div className="not__found__main">
+        <img src="/logo.png" alt="logo" />
+        <h1>
+          <span>4</span>
+          <span>0</span>
+          <span>4</span>
+        </h1>
+        <p>Not Found!</p>
+        <div className="not__found__links">
+          <Link to={"/"}>HOME</Link>
+          <Link to={"/"}>LOGIN</Link>
+          <Link to={"/"}>REGISTER</Link>
+        </div>
+      </div>
     </div>
   );
 };
